8ddf25f326f8f5623eeda6073d5c0593bd16abc6
Poisedboy <beschasnuy96@gmail.com>
2025-03-29 17:18:28 +0200

    made middleware protected routes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,16 +1,10 @@
-import { getServerSession } from "next-auth";
-
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession();
   return (
     <div className="flex flex-col min-h-screen">
-      <header className=" p-4">
-        <h1>Welcome back, {session?.user.email}!</h1>
-      </header>
       <main className="flex-1 p-4">{children}</main>
     </div>
   );
