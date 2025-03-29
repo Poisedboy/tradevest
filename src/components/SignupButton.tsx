5c9f968e17e16e116b04f80fb05222650fcc0405
Poisedboy <beschasnuy96@gmail.com>
2025-03-29 07:12:17 +0200

    added shadcn ui, changed landing page

---
+++ src/components/SignupButton.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+
+export function SignUpButton() {
+  const router = useRouter();
+
+  return (
+    <Button size={"lg"} onClick={() => router.push("/signup")}>
+      Sign up
+    </Button>
+  );
+}
