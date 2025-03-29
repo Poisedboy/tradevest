16d54c2b8d0c7965a85fca14242e93c11a507b33
Poisedboy <beschasnuy96@gmail.com>
2025-03-29 22:32:12 +0200

    setup eslint, prettier, husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,82 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import react from "eslint-plugin-react";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  {
+    ignores: [
+      "**/node_modules/*",
+      "**/out/*",
+      "**/.next/*",
+      "**/coverage",
+      "src/styles/globals.css",
+    ],
+  },
+  ...compat.extends(
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ),
+  {
+    plugins: {
+      react,
+      "@typescript-eslint": typescriptEslint,
+    },
 
-export default eslintConfig;
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 11,
+      sourceType: "module",
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    rules: {
+      "react/prop-types": 0,
+      "react/react-in-jsx-scope": 0,
+      "@typescript-eslint/no-unused-vars": [
+        2,
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "no-console": [
+        2,
+        {
+          allow: ["warn", "error"],
+        },
+      ],
+    },
+  },
+];
